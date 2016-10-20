d154195ffa3114af9f9b615389df816a2219e7bc
Manigandancommit <Manigandan@DESKTOP-1EQK1RR.mshome.net>
2016-10-20 15:39:43 +0530

    3

--- sample/src/hello/FirstProg.java
+++ sample/src/hello/FirstProg.java
@@ -4,7 +4,7 @@ public class FirstProg {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("My first program2");
+System.out.println("My first program3");
 	}
 
 }
